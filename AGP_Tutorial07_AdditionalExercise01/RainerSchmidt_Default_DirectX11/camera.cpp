87fba167de00ff9496b71b920f11cb862ccb4ec0
rainerSchmidt <32327966+rainerSchmidt@users.noreply.github.com>
2017-11-01 10:46:45 +0000

    Add files via upload

---
+++ AGP_Tutorial07_AdditionalExercise01/RainerSchmidt_Default_DirectX11/camera.cpp
@@ -0,0 +1,60 @@
+#include "camera.h"
+#include <math.h>
+
+	Camera::Camera(float cameraPositionX, float cameraPositionY, float cameraPositionZ, float cameraRotation)
+	{
+		m_x = cameraPositionX;
+		m_y = cameraPositionY;
+		m_z = cameraPositionZ;
+		m_camera_rotation = cameraRotation;
+
+		m_dx = sin(m_camera_rotation * (XM_PI / 180.0));
+		m_dz = cos(m_camera_rotation * (XM_PI / 180.0));
+	}
+
+	Camera::~Camera()
+	{
+		
+	}
+
+	void Camera::Rotate(float amountInDegrees)
+	{
+		m_camera_rotation += amountInDegrees;
+
+		m_dx = sin(m_camera_rotation * (XM_PI / 180.0));
+		m_dz = cos(m_camera_rotation * (XM_PI / 180.0));
+	}
+
+	void Camera::Forward(float distance)
+	{
+		m_x += m_dx * distance;
+		m_z += m_dz * distance;
+	}
+
+	void Camera::Up()
+	{
+		 
+	}
+
+	void Camera::Strafe(float objectPositionZ, float objectPositionX, float forwardAmount)
+	{
+		// rotate camera
+		//Rotate(-1 * forwardAmount);
+		// move camera
+		m_dx += forwardAmount;
+		Rotate(-1*(atan((m_dz / m_dx)*(XM_PI * 180))));
+
+		
+	}
+
+	XMMATRIX Camera::GetViewMatrix()
+	{
+		m_position = XMVectorSet(m_x, m_y, m_z, 0.0);
+		m_lookat = XMVectorSet(m_x + m_dx, m_y, m_z + m_dz, 0.0);
+		m_up = XMVectorSet(0.0, 1.0, 0.0, 0.0);
+
+		XMMATRIX view = XMMatrixLookAtLH(m_position, m_lookat, m_up);
+
+		return view;
+	}
+	 
---
+++ AGP_Tutorial07_AdditionalExercise01/RainerSchmidt_Default_DirectX11/camera.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <d3d11.h>
+#define _XM_NO_INTINSICS_
+#define XM_NO_ALIGNMENT
+#include <xnamath.h>
+
+class Camera
+{
+	public: 
+		Camera(float cameraPositionX, float cameraPositionY, float cameraPositionZ, float cameraRotation);
+		~Camera();
+		void Rotate(float amountInDegrees);
+		void Forward(float distance);
+		void Up();
+		void Strafe(float objectPositionZ, float objectPositionX, float forwardAmount);
+
+		XMMATRIX GetViewMatrix();
+	private:
+		float m_x, m_y, m_z, m_dx, m_dz, m_camera_rotation=0.0f;
+		XMVECTOR m_position, m_lookat, m_up;
+};
